9b0e79f0673c068020a757171ff4df69284560cf
Sebastian Araque <cbastian97araque@gmail.com>
2025-11-04 22:53:39 -0500

    # Cambio al título del index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Bienvenido al sistema de registro</h1>";
+echo "<h1>Aplicación de registro de usuarios</h1>";
 echo "<p><a href='register.php'>Ir a registro</a></p>";
 echo "<p><a href='login.php'>Ir a inicio de sesión</a></p>";
 ?>
